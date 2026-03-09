3d33e95857abd1013f2b0be444b82b48e9c15ee8
Александра Чернова <aleksandrachernova841@gmail.com>
2026-03-09 19:40:35 +0300

    fix alembic env for render

--- alembic/env.py
+++ alembic/env.py
@@ -12,10 +12,16 @@ from app.db.models import Base
 import app.db.models
 import app.auth.db
 
+import os
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
+
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    database_url = database_url.replace("postgres://", "postgresql+asyncpg://")
+    database_url = database_url.replace("postgresql://", "postgresql+asyncpg://")
+
+    config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
